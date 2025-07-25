4f5a7c4b978e0817375c3253c43efdb981eb16ab
amiteshyadav3333 <yadavamiteshkumar11@gmail.com>
2025-07-26 00:51:35 +0530

    Initial commit for LiveCast ENJOYABLE

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Go to project root (where this script is located)
-cd "$(dirname "$0")"
+# run.sh
+cd backend
+gunicorn app:app --worker-class eventlet -w 1 --bind 0.0.0.0:$PORT
 
 # Activate virtual environment
 source venv/bin/activate
